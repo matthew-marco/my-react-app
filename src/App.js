8e31689b33fa72ee738fbbb01ac1a800e58cf192
Matthew <matthewmarcoy168@gmail.com>
2023-04-23 08:38:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="title">c4a.dev console.log(hello)</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse cumque
+        officiis ullam, maiores expedita dolores dolore corporis velit
+        perspiciatis inventore et sit facere qui earum blanditiis. Numquam,
+        aperiam repudiandae? Cumque.
+      </p>
+    </>
+  );
+}
+
+export default App;
